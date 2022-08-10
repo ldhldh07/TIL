c68bb190c94ef93732a55caf2b4f947ecc7d0a68
ldhldh07 <dhlim715@gmail.com>
2022-08-11 01:44:50 +0900

    add 08/10 NT

--- python03_ds.py
+++
@@ -1,7 +0,0 @@
-import copy
-
-a = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
-b = copy.deepcopy(a)
-print(a,b) #[1, 2, ['a', 'b']] [1, 2, ['a', 'b']]
-b[0][2] = 'hello'
-print(a,b) #[1, 2, [5, 'b']] [1, 2, [5, 'b']]
