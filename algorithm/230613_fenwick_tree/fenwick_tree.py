95205c6259551da0078d9442af130ac175720109
임두현 <dhlim07@naver.com>
2023-06-13 01:48:01 +0900

    add 230613 algorithm NT

---
+++ algorithm/230613_fenwick_tree/fenwick_tree.py
@@ -0,0 +1,16 @@
+class FenwickTree:
+    def __init__(self, size):
+        self.size = size
+        self.tree = [0 for _ in range(size+1)]
+    
+    def update(self, value):
+        while value <= self.size:
+            self.tree[value] += 1
+            value += value & -value 
+    
+    def query(self, i):
+        cumulative_sum = 0
+        while i > 0:
+            cumulative_sum += self.tree[i]
+            i -= i & -i
+        return cumulative_sum
