d49980adfca63090c0167467fb0a79ca54f43197
임두현 <dhlim07@naver.com>
2023-07-05 16:20:02 +0900

    add 230705 NT

---
+++ algorithm/230705_disjoint_set/disjoint_set.py
@@ -0,0 +1,21 @@
+class DisjointSet:
+    def __init__(self, n):
+        self.parent = list(range(n))
+        self.rank = [0] * n
+    
+    def find(self, x):
+        if self.parent[x] != x:
+            self.parent[x] = self.find(self.parent[x])
+        return self.parent[x]
+    
+    def union(self, x, y):
+        x_root = self.find(x)
+        y_root = self.find(y)
+
+        if x_root != y_root:
+            if self.rank[x_root] < self.rank[y_root]:
+                self.parent[x_root] = y_root
+            else:
+                self.parent[y_root] = x_root
+                if self.rank[x_root] == self.rank[y_root]:
+                    self.rank[x_root] += 1
