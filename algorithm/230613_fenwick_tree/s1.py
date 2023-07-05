070f7106ec278aa3d08165c5d13106fb0aa85c0d
임두현 <dhlim07@naver.com>
2023-07-05 16:19:12 +0900

    add 230613 NT

--- algorithm/230613_fenwick_tree/s1.py
+++ algorithm/230613_fenwick_tree/s1.py
@@ -14,20 +14,3 @@ class FenwickTree:
             cumulative_sum += self.tree[i]
             i -= i & -i
         return cumulative_sum
-    
-
-'''
-https://www.acmicpc.net/blog/view/21
-'''
-
-arr = [1, 3, 5, 3, 1, 3]
-fenwick_tree = FenwickTree(len(arr))
-
-print(fenwick_tree.tree)
-fenwick_tree.update(1) # 1
-print(fenwick_tree.tree)
-fenwick_tree.update(3) # 3
-print(fenwick_tree.tree)
-fenwick_tree.update(5) # 5
-print(fenwick_tree.tree)
-print(fenwick_tree.query(4))
