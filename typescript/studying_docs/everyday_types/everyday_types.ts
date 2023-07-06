b78b4126bf156bd050698a98d688edb3a5b447a1
임두현 <dhlim07@naver.com>
2023-07-06 17:37:08 +0900

    add Typescript docs NT

--- typescript/studying_docs/everyday_types/everyday_types.ts
+++ typescript/studying_docs/everyday_types/everyday_types.ts
@@ -1,1 +1,16 @@
 const numbs: number[]= [1, 2]
+
+declare function getInput(): string;
+declare function sanitize(str: string): string;
+// ---cut---
+type UserInputSanitizedString = string;
+
+function sanitizeInput(str: string): UserInputSanitizedString {
+  return sanitize(str);
+}
+
+// 보안 처리를 마친 입력을 생성
+let userInput = sanitizeInput(getInput());
+
+// 물론 새로운 문자열을 다시 대입할 수도 있습니다
+userInput = "new input";
